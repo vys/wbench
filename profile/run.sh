54568b0ecac3e09a2119078404a20e2ba00d0acb
Vinay Y S <vinay@vys.in>
2012-11-18 02:18:41 +0530

    Fixed run.sh
    Signed-off-by: Vinay Y S <vinay@vys.in>

--- profile/run.sh
+++ profile/run.sh
@@ -1,8 +1,17 @@
-#!/bin/bash -xe
+#!/bin/bash -x
+pkill -9 redis-server
+pkill -9 ab
+pkill -9 wbench
+ 
 redis-server > redis.log 2>&1 &
+sleep 3
 ../wbench -profile web > wbench.log 2>&1 &
+sleep 2
 curl http://localhost:8081/reset > populate.log 2>&1
+sleep 2
 curl http://localhost:8081/test >> populate.log 2>&1
-ab -c 100 -n 10000000 -r http://localhost:8081/
-go tool pprof --pdf wbench wbench-cpu-1.prof > profile-cpu.pdf
-go tool pprof --pdf wbench wbench-heap-1.prof > profile-heap.pdf
+sleep 2
+ab -c 100 -n 1000000 -r http://localhost:8081/ > ab.log 2>&1 
+sleep 10
+go tool pprof --pdf ../wbench web-cpu-1.prof > profile-cpu.pdf
+go tool pprof --pdf ../wbench web-heap-1.prof > profile-heap.pdf
